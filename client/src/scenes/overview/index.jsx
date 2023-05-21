86feb091721b48eaf361124acfdc6bc5c35e9836
anhhpdev <phucanh.dev@gmail.com>
2023-05-21 22:00:03 +0700

    up dashboard

---
+++ client/src/scenes/overview/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { FormControl, MenuItem, InputLabel, Box, Select } from "@mui/material";
+import Header from "components/Header";
+import OverviewChart from "components/OverviewChart";
+
+const Overview = () => {
+  const [view, setView] = useState("units");
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header
+        title="TỔNG QUAN"
+        subtitle="Tổng quan về doanh thu và lợi nhuận chung"
+      />
+      <Box height="75vh">
+        <FormControl sx={{ mt: "1rem" }}>
+          <InputLabel>View</InputLabel>
+          <Select
+            value={view}
+            label="View"
+            onChange={(e) => setView(e.target.value)}
+          >
+            <MenuItem value="sales">Doanh thu</MenuItem>
+            <MenuItem value="units">Doanh số</MenuItem>
+          </Select>
+        </FormControl>
+        <OverviewChart view={view} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Overview;
